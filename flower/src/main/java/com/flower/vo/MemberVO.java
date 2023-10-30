5568432ab7a0c519d3648fb9540af271a52e3966
ict0323 <ict0323@114.207.167.88>
2023-10-30 16:54:10 +0900

    memberVO완성

--- flower/src/main/java/com/flower/vo/MemberVO.java
+++ flower/src/main/java/com/flower/vo/MemberVO.java
@@ -2,4 +2,11 @@ package com.flower.vo;
 
 public class MemberVO {
 
+	private Integer member_id;
+	private boolean admin_status;
+	private String member_email;
+	private String member_password;
+	private String member_name;
+	private String member_register_date;
+	private String member_leave_date;
 }
